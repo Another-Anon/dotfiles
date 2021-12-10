6b89a5c5fdf2e77e692fb7789349fb02386e2263
Dasha <sudasha42@gmail.com>
2021-12-10 06:10:20 +0200

    auto-commit

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -82,7 +82,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static const char *waterfox[] = { "waterfox", NULL};
 static const char *telegram[] = { "telegram-desktop", NULL};
